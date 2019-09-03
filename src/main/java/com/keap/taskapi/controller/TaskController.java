9f887e452a30f1a06b97301a70e280dc78c5a05a
sdanyalk <s.danyal.k@gmail.com>
2019-09-03 16:59:40 -0700

    Add dummy API and config for GCP

--- src/main/java/com/keap/taskapi/controller/TaskController.java
+++ src/main/java/com/keap/taskapi/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.keap.taskapi.controller;
 import com.keap.taskapi.model.Task;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TaskController {
 
-    @RequestMapping(
-            path = "/tasks",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+//    @RequestMapping(
+//            path = "/tasks",
+//            method = RequestMethod.GET,
+//            produces = MediaType.APPLICATION_JSON_VALUE)
 
+    @GetMapping(
+            path = "/tasks",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<Task> getTasks() {
         Task task = new Task();
         task.setId(1);
         task.setDescription("complete the project");
 
         return ResponseEntity.ok(task);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot!";
     }
 }
