2ebd6f45093a94a21a5d4b494a20129ede1d577e
sdanyalk <s.danyal.k@gmail.com>
2019-10-25 10:06:32 -0700

    add unit test

--- src/test/java/com/keap/taskapi/controller/TaskControllerTest.java
+++ src/test/java/com/keap/taskapi/controller/TaskControllerTest.java
@@ -8,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.containsString;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -32,12 +35,21 @@ class TaskControllerTest {
     }
 
     @Test
-    void getTasks() throws Exception {
+    void testTasksEndpoint() throws Exception {
+        this.
+                mockMvc
+                .perform(get("/tasks"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description", is("complete the project")));
     }
 
     @Test
-    void hello() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+    void testDummyEndpoint() throws Exception {
+        this.
+                mockMvc
+                .perform(get("/"))
+                .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello World")));
     }
 }
