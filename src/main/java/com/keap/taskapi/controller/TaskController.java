7e9c0773622d6b4d23dc3fbd63461a1f006a2a56
sdanyalk <s.danyal.k@gmail.com>
2019-10-25 09:42:44 -0700

    add test

--- src/main/java/com/keap/taskapi/controller/TaskController.java
+++ src/main/java/com/keap/taskapi/controller/TaskController.java
@@ -25,6 +25,6 @@ public class TaskController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Spring Boot!";
+        return "Hello World";
     }
 }
---
+++ src/test/java/com/keap/taskapi/controller/TaskControllerTest.java
@@ -0,0 +1,43 @@
+package com.keap.taskapi.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getTasks() throws Exception {
+    }
+
+    @Test
+    void hello() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+}
