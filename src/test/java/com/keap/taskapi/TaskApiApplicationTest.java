ba2f46c35fac0e327b74b3a2611f37f9a1e7105e
sdanyalk <s.danyal.k@gmail.com>
2019-10-25 10:24:21 -0700

    add main unit test

---
+++ src/test/java/com/keap/taskapi/TaskApiApplicationTest.java
@@ -0,0 +1,23 @@
+package com.keap.taskapi;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskApiApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        TaskApiApplication.main(new String[]{});
+    }
+}
